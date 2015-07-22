adcf62258c89e119c85e0b392810b1692104a58b
h136799711 <hebiduhebi@126.com>
2015-07-22 20:33:49 +0800

    更新

--- Application/Shop/Conf/config.php
+++ Application/Shop/Conf/config.php
@@ -19,17 +19,18 @@ return array(
 
     'WXPAY_CONFIG'=>array(
 
-		'APPID'=>'wx3fe04f32017f50a5',
-		'APPSECRET'=>'f7dbb6d7882ecaa984a9f3e900db9a3d',
-		'MCHID'=>'1237211302',
-		'KEY'=>'c01843987e76de691cc94dbb402fe2aa',//在微信发送的邮件中查看,patenerkey
+		'APPID'=>'wx5f75a875d7bb9c28',
+		'APPSECRET'=>'818dec3ac3c3ba43e4ca018f4fd77553',
+		'MCHID'=>'1250762701',
+		'KEY'=>'E48D86C64D6B8EA672BEBF6ACC75AD94',//在微信发送的邮件中查看,patenerkey
 //
-		'NOTIFYURL'=>'http://renren.itboye.com/index.php/Shop/WxpayNotify/index',
+		'NOTIFYURL'=>'http://shuiguo.itboye.com/index.php/Shop/WxpayNotify/index',
 		'JSAPICALLURL'=>'http://renren.itboye.com/index.php/Shop/Orders/pay?showwxpaytitle=1',
 		'SSLCERTPATH'=>'/alidata/8rawcert/10027619/apiclient_cert.pem',
 		'SSLKEYPATH'=>'/alidata/8rawcert/10027619/apiclient_cert.pem',
 		'CURL_PROXY_HOST' => "0.0.0.0",
 		'CURL_PROXY_PORT' => '0',
-		'REPORT_LEVENL' => 1
+		'REPORT_LEVENL' => 1,
+		'PROCESS_URL'=>'http://shuiguo.itboye.com/index.php/Shop/WxpayNotify/aysncNotify?key=hebidu',//异步处理地址
     ),
 );
